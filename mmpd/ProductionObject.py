30ed9bfa2d718e0be17e0b29a471a15d79b33f55
luca-aussem <luca-aussem@t-online.de>
2025-07-16 16:11:16 +0200

    Changed: Reduced needed attribute logic

--- mmpd/ProductionObject.py
+++ mmpd/ProductionObject.py
@@ -1,9 +1,9 @@
-from typing import Union
+from typing import Union, Any
 
 ALLOWED_TYPES = Union[str, int, float]
 
 class ProductionObject:
-    __attributes: dict[str, dict[str, Union[str, int, float]]]
+    __attributes: dict[str, Any]
     
     def __init__(self) -> None:
         self.__attributes = {}
@@ -12,11 +12,8 @@ class ProductionObject:
         if not isinstance(value, (str, int, float)):
             raise TypeError(f"Value {value!r} must be str, int, or float")
 
-        self.__attributes[key] = {
-            'value': value,
-            'type': type(value).__name__
-        }
+        self.__attributes[key] = value
 
     @property
-    def attributes(self) -> dict[str, dict[str, Union[str, int, float]]]:
+    def attributes(self) -> dict[str, Any]:
         return self.__attributes
