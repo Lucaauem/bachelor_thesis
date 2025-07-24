ffae6a0cacf2104bfed52fc599f1472b120cf0f8
Luca.aue <luca-aussem@t-online.de>
2025-07-24 21:41:30 +0200

    Updated: Callbacks

--- database/callbacks/CallbackHandler.py
+++ database/callbacks/CallbackHandler.py
@@ -13,6 +13,6 @@ class CallbackHandler:
     def add(self, function: Callable, event: CallEvent) -> None:
         self._callbacks[event].append(function)
 
-    def trigger(self, event: CallEvent) -> None:
+    def trigger(self, event: CallEvent, *args, **kwargs) -> None:
         for callable in self._callbacks[event]:
-            callable()
+            callable(*args, **kwargs)
--- database/callbacks/Events.py
+++ database/callbacks/Events.py
@@ -1,8 +1,8 @@
 from enum import Enum, auto
 
 class Events(Enum):
-    STARTUP = auto(),
-    SHUTDOWN = auto(),
-    NEW_SENSOR_READING = auto(),
-    MODEL_UPDATE = auto(),
+    STARTUP = auto()
+    SHUTDOWN = auto()
+    NEW_SENSOR_READING = auto()
+    MODEL_UPDATE = auto()
     INVALID_DATASET = auto()
