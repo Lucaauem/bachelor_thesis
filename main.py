fc479585a7aacac41880b8bc49e879e268cc72b5
Luca.aue <luca-aussem@t-online.de>
2025-07-27 23:35:22 +0200

    Added: Demo process

--- main.py
+++ main.py
@@ -1,121 +1,20 @@
-from datamodel.mmpd.process.ProcessStepSpecification import ProcessStepSpecification
-from datamodel.mmpd.product.ProductSpecification import ProductSpecification
-from datamodel.mmpd.process.Operator import Operator
-from datamodel.mmpd.product.Product import Product
-from datamodel.mmpd.product.PreProduct import PreProduct
-from datamodel.mmpd.product.Batch import Batch
-from datamodel.soil.SensorReading import SensorReading
-from datamodel.soil.Component import Component
-from datamodel.soil.ComponentType import ComponentType
-from datamodel.mmpd.process.ProcessStep import ProcessStep
-from datamodel.mmpd.resource.Machine import Machine
-from datamodel.mmpd.resource.ShopFloor import ShopFloor
-from datamodel.Model import Model
+from model import create_model
 from database.DBFramework import DBFramework
-import os
 import json
 
-DUMMIES_DIR = './dummies'
-OUTPUT_DIR = './output'
-
 def main():
-    model = create_dummy_model()
+    model = create_model()
+    app = DBFramework()
 
-    if not os.path.exists(OUTPUT_DIR):
-        os.makedirs(OUTPUT_DIR)
-
-    with open(f'{OUTPUT_DIR}/output.json', 'w') as f:
-        json.dump(model.serialize(), f, indent=2)
-
-def create_dummy_model() -> Model:
-    with open(f'{DUMMIES_DIR}/soil_dummy_mea.json') as f:
-        dummy_sr = f.read()
-    with open(f'{DUMMIES_DIR}/soil_dummy_sensor.json') as f:
-        dummy_sensor = f.read()
-    with open(f'{DUMMIES_DIR}/soil_dummy_sensor_2.json') as f:
-        dummy_sensor_2 = f.read()
-    with open(f'{DUMMIES_DIR}/soil_dummy_tool.json') as f:
-        dummy_tool = f.read()
-    with open(f'{DUMMIES_DIR}/soil_dummy_tool_2.json') as f:
-        dummy_tool_2 = f.read()
-    with open('sensors/lasertracker.json') as f:
-        lasertracker = f.read()
-
-    model = Model()
-
-    p_1 = Product('P_1', model)
-    p_2 = Product('P_2', model)
-
-    batch = Batch('BATCH', model)
-    batch.add_product(p_1)
-    batch.add_product(p_2)
-
-    psp = ProductSpecification('PSP', model)
-    p_1.specification = psp
-    p_2.specification = psp
-
-    pp_1 = PreProduct('PP_1', model)
-    pss_1 = ProcessStepSpecification('PSS_1', model)
-    ps_1 = ProcessStep('PS_1', model)
-    ps_1.specification = pss_1
-    ps_1.pre_product = pp_1
-    ps_1.product = p_1
-
-    pp_2 = PreProduct('PP_2', model)
-    pss_2 = ProcessStepSpecification('PSS_2', model)
-    ps_2 = ProcessStep('PS_2', model)
-    ps_2.specification = pss_2
-    ps_2.pre_product = pp_2
-    ps_2.product = p_2
-
-    ps_1.next_step = ps_2
-
-    operator = Operator('OPERATOR', model)
-    ps_1.operator = operator
-    ps_2.operator = operator
-
-    tool_1 = Component(dummy_tool, ComponentType.TOOL, model)
-    tool_2 = Component(dummy_tool_2, ComponentType.TOOL, model)
-    ps_1.tool = tool_2
-    ps_2.tool = tool_1
-
-    #s_acc_nozzle = Component(dummy_sensor, ComponentType.REAL, model)
-    #s_acc_bed = Component(dummy_sensor, ComponentType.REAL, model)
-    #s_diameter = Component(dummy_sensor_2, ComponentType.REAL, model)
-    s_lasertracker = Component(lasertracker, ComponentType.REAL, model)
-
-    sr_acc_nozzle = SensorReading(dummy_sr)
-    sr_acc_bed = SensorReading(dummy_sr)
-    sr_diameter = SensorReading(dummy_sr)
-
-    """
-    s_acc_nozzle.add_reading(sr_acc_nozzle)
-    s_acc_bed.add_reading(sr_acc_bed)
-    s_diameter.add_reading(sr_diameter)
-
-    ps_1.add_sensor_reading(sr_acc_nozzle)
-    ps_1.add_sensor_reading(sr_acc_bed)
-    ps_1.add_sensor_reading(sr_diameter)
-    ps_2.add_sensor_reading(sr_acc_nozzle)
-    ps_2.add_sensor_reading(sr_acc_bed)
-    ps_2.add_sensor_reading(sr_diameter)
-    """
-
-    printer_1 = Machine('PRINTER_1', model)
-    printer_1.add_tool(tool_1)
-    printer_1.add_tool(tool_2)
-    #printer_1.add_sensor(s_acc_nozzle)
-    #printer_1.add_sensor(s_diameter)
-    printer_1.add_sensor(s_lasertracker)
-    ps_1.machine = printer_1
-    ps_2.machine = printer_1
-
-    shop_floor = ShopFloor('SHOP_FLOOR', model)
-    shop_floor.add_machine(printer_1)
-
-    p_1.set_attributes(name='Lamp', costs=100, quality=0.73)
-
-    return model
+    # Framework configuration
+    tsdb = app.DB.add_tsdb('./components/databases/influx.toml')
+    graphdb = app.DB.add_graphdb('./components/databases/neo.toml')
+    app.DB.set_tsdb(tsdb)
+    app.DB.set_graphdb(graphdb)
+    app.clear_model()
+    app.set_model(json.dumps(model.serialize()))
+    #mqtt = app.MQTT.add_client('./components/mqtt/client.toml')
+    #app.launch()
 
 if __name__ == '__main__':
     main()
---
+++ model.py
@@ -0,0 +1,118 @@
+from datamodel.mmpd.process.ProcessStepSpecification import ProcessStepSpecification
+from datamodel.mmpd.product.ProductSpecification import ProductSpecification
+from datamodel.mmpd.process.Operator import Operator
+from datamodel.mmpd.product.Product import Product
+from datamodel.mmpd.product.PreProduct import PreProduct
+from datamodel.mmpd.product.Batch import Batch
+from datamodel.soil.SensorReading import SensorReading
+from datamodel.soil.Component import Component
+from datamodel.soil.ComponentType import ComponentType
+from datamodel.mmpd.process.ProcessStep import ProcessStep
+from datamodel.mmpd.resource.Machine import Machine
+from datamodel.mmpd.resource.ShopFloor import ShopFloor
+from datamodel.Model import Model
+
+def create_model() -> Model:
+    model = Model()
+
+    # Machines, Components, ...
+    shopfloor_0 = ShopFloor('SHOP_FLOOR_1', model)
+    shopfloor_1 = ShopFloor('SHOP_FLOOR_2', model)
+
+    machine_0 = Machine('MACHINE_1', model)
+    machine_0.set_attributes(age=15, usages=673)
+    machine_1 = Machine('MACHINE_2', model)
+    machine_1.set_attributes(age=5, usages=902)
+    machine_2 = Machine('MACHINE_3', model)
+    machine_2.set_attributes(age=2, usages=157)
+
+    # TODO Components
+
+    shopfloor_0.add_machine(machine_0)
+    shopfloor_0.add_machine(machine_1)
+    shopfloor_1.add_machine(machine_2)
+
+    # Products, etc.
+    pre_product_0 = PreProduct('PP_1', model)
+    pre_product_0.set_attributes(name='Copper', cost=10, ammount=2)
+    pre_product_specification_0 = ProductSpecification('PSP_P1', model)
+    pre_product_specification_0.set_attributes(weight=0.01)
+    pre_product_specification_0.add_to_pre_product(pre_product_0)
+
+    product_0 = Product('P_1', model)
+    product_0.set_attributes(name='Wire', quality=0.5)
+    product_specification_0 = ProductSpecification('PSP_1', model)
+    product_specification_0.set_attributes(weight=0.01)
+    product_specification_0.add_to_product(product_0)
+
+
+    pre_product_1 = PreProduct('PP_2', model)
+    pre_product_1.set_attributes(name='Wire', cost=20, ammount=6)
+    pre_product_specification_1 = ProductSpecification('PSP_P2', model)
+    pre_product_specification_1.set_attributes(weight=0.015)
+    pre_product_specification_1.add_to_pre_product(pre_product_1)
+    
+    product_1 = Product('P_2', model)
+    product_1.set_attributes(name='Spoil', cost=40, quality=0.84)
+    product_specification_1 = ProductSpecification('PSP_2', model)
+    product_specification_1.set_attributes(weight=0.015)
+    product_specification_1.add_to_product(product_1)
+
+
+    pre_product_2 = PreProduct('PP_3', model)
+    pre_product_2.set_attributes(name='Spoil', cost=20, ammount=6)
+    pre_product_specification_2 = ProductSpecification('PSP_P3', model)
+    pre_product_specification_2.set_attributes(weight=0.0175)
+    pre_product_specification_2.add_to_pre_product(pre_product_2)
+
+    product_2 = Product('P_3', model)
+    product_2.set_attributes(name='Circuit', cost=75, quality=0.92)
+    specification_2 = ProductSpecification('PSP_3', model)
+    specification_2.set_attributes(weight=0.0175)
+    specification_2.add_to_product(product_2)
+
+    batch = Batch('B', model)
+    batch.set_attributes(cost=150, max_weight=0.25)
+    batch.add_product(product_0)
+    batch.add_product(product_1)
+    batch.add_product(product_2)
+
+
+    # Production steps
+    operator = Operator('O', model)
+
+    process_step_0 = ProcessStep('PS_1', model)
+    process_step_specification_0 = ProcessStepSpecification('PSP_1', model)
+    process_step_specification_0.set_attributes(duration=60)
+    process_step_specification_0.add_to_step(process_step_0)
+
+    operator.add_to_step(process_step_0)
+    pre_product_0.add_to_step(process_step_0)
+    product_0.add_to_step(process_step_0)
+    process_step_0.machine = machine_0
+
+
+    process_step_1 = ProcessStep('PS_2', model)
+    process_step_specification_1 = ProcessStepSpecification('PSP_2', model)
+    process_step_specification_1.set_attributes(duration=30)
+    process_step_specification_1.add_to_step(process_step_1)
+
+    operator.add_to_step(process_step_1)
+    pre_product_1.add_to_step(process_step_1)
+    product_1.add_to_step(process_step_1)
+    process_step_1.machine = machine_1
+
+    process_step_2 = ProcessStep('PS_3', model)
+    process_step_specification_2 = ProcessStepSpecification('PSP_3', model)
+    process_step_specification_2.set_attributes(duration=360)
+    process_step_specification_2.add_to_step(process_step_2)
+
+    operator.add_to_step(process_step_2)
+    pre_product_2.add_to_step(process_step_2)
+    product_2.add_to_step(process_step_2)
+    process_step_2.machine = machine_2
+
+    process_step_0.next_step = process_step_1
+    process_step_1.next_step = process_step_2
+
+    return model
